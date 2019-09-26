27dfc2245e48192c06ca681f733ac929e932ba9a
Andrew M. Bradley <ambradl@sandia.gov>
2019-09-26 14:53:35 -0600

    start working on higher order eta_dot_dpdn reconstruction again

--- components/homme/src/share/sl_advection.F90
+++ components/homme/src/share/sl_advection.F90
@@ -153,6 +153,9 @@ contains
           dp = elem(ie)%state%dp3d(:,:,:,tl%np1)
           ! use divdp for dp_star
           if (rsplit == 0) then
+             call reconstruct_eta_dot_dpdn(hvcoord, dt, &
+                  elem(ie)%state%dp3d(:,:,:,tl%n0), elem(ie)%state%dp3d(:,:,:,tl%np1), &
+                  elem(ie)%derived%eta_dot_dpdn)
              elem(ie)%derived%divdp = dp + &
                   dt*(elem(ie)%derived%eta_dot_dpdn(:,:,2:) - elem(ie)%derived%eta_dot_dpdn(:,:,1:nlev))
           else
@@ -776,5 +779,88 @@ contains
 #endif
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   end subroutine biharmonic_wk_scalar
+
+  subroutine eval_lagrange_poly(n, xs, ys, xi, y)
+    integer, intent(in) :: n
+    real(kind=real_kind), intent(in) :: xs(:), ys(:), xi
+    real(kind=real_kind), intent(out) :: y
+
+    integer :: i, j
+    real(kind=real_kind) :: f
+
+    y = 0
+    do i = 1,n
+       f = 1
+       do j = 1,n
+          if (i == j) cycle
+          f = f*((xi - xs(j))/(xs(i) - xs(j)))
+       end do
+       y = y + ys(i)*f
+    end do
+  end subroutine eval_lagrange_poly
+
+  subroutine interp(n, x, y, xi, yi)
+    integer, intent(in) :: n
+    real(kind=real_kind), intent(in) :: x(:), y(:), xi(:)
+    real(kind=real_kind), intent(out) :: yi(:)
+
+    real(kind=real_kind) :: alpha
+    integer :: j, ji
+
+    j = 1
+    ji = 1
+    do while (ji <= n)
+       if (j < n-1 .and. xi(ji) > x(j+1)) then
+          j = j + 1
+       else
+#if 0
+          alpha = (xi(ji) - x(j))/(x(j+1) - x(j))
+          yi(ji) = (1 - alpha)*y(j) + alpha*y(j+1)
+#else
+          if (j == 1) then
+             call eval_lagrange_poly(3, x(j:j+2), y(j:j+2), xi(ji), yi(ji))
+          elseif (j == n-1) then
+             call eval_lagrange_poly(3, x(j-1:j+1), y(j-1:j+1), xi(ji), yi(ji))
+          else
+             call eval_lagrange_poly(4, x(j-1:j+2), y(j-1:j+2), xi(ji), yi(ji))
+          end if
+#endif
+          ji = ji + 1
+       end if
+    end do
+  end subroutine interp
+  
+  subroutine reconstruct_eta_dot_dpdn(hvcoord, dt, dp0, dp1, eta_dot_dpdn)
+    type (hvcoord_t), intent(in) :: hvcoord
+    real(kind=real_kind), intent(in) :: dt, dp0(np,np,nlev), dp1(np,np,nlev)
+    real(kind=real_kind), intent(inout) :: eta_dot_dpdn(np,np,nlevp)
+
+    real(kind=real_kind) :: p0(np,np,nlevp), p1(np,np,nlevp), pr(np,np,nlevp), &
+         ph0(np,np,nlevp), eta_dot_dpdn_h0(np,np,nlevp), eta_dot_dpdn_h(np,np,nlevp), tmp(np,np,nlevp)
+    integer :: k, nit, i, j
+
+    p0(:,:,1) = 0
+    p1(:,:,1) = 0
+    do k = 2,nlevp
+       p0(:,:,k) = p0(:,:,k-1) + dp0(:,:,k-1)
+       p1(:,:,k) = p1(:,:,k-1) + dp1(:,:,k-1)
+    end do
+
+    ph0 = 0.5d0*(p0 + p1)
+    eta_dot_dpdn_h0 = eta_dot_dpdn
+    eta_dot_dpdn_h = eta_dot_dpdn_h0
+    nit = 0
+    do k = 1,nit
+       pr = p0 + 0.5d0*dt*eta_dot_dpdn_h
+       !tmp = eta_dot_dpdn_h
+       do j = 1,np
+          do i = 1,np
+             call interp(nlevp, ph0(i,j,:), eta_dot_dpdn_h0(i,j,:), pr(i,j,:), eta_dot_dpdn_h(i,j,:))
+          end do
+       end do
+       !print *,k,sum((eta_dot_dpdn_h - tmp)**2)
+    end do
+    eta_dot_dpdn = eta_dot_dpdn_h
+  end subroutine reconstruct_eta_dot_dpdn
 
 end module sl_advection
