b79af221c0a75e0faac3eef7c9620d2def8f7886
Andrew M. Bradley <ambradl@sandia.gov>
2019-12-23 18:58:14 -0700

    factor out scalar computation in EOS

--- components/homme/src/theta-l_kokkos/cxx/EquationOfState.hpp
+++ components/homme/src/theta-l_kokkos/cxx/EquationOfState.hpp
@@ -156,18 +156,24 @@ public:
 
     // Use ColumnOps to do the scan sum
     auto integrand_provider = [&](const int ilev)->Scalar {
-      constexpr Real p0    = PhysicalConstants::p0;
-      constexpr Real kappa = PhysicalConstants::kappa;
-      constexpr Real Rgas  = PhysicalConstants::Rgas;
-#ifdef HOMMEXX_BFB_TESTING
-      return (Rgas*vtheta_dp(ilev) * bfb_pow(p(ilev)/p0,kappa-1)) / p0;
-#else
-      // TODO: remove temporaries
-      return (Rgas*vtheta_dp(ilev) * pow(p(ilev)/p0,kappa-1)) / p0;
-#endif
+      return compute_dphi(vtheta_dp(ilev), p(ilev));
     };
 
     ColumnOps::column_scan_mid_to_int<false>(kv,integrand_provider,phi_i);
+  }
+
+  template<typename Scalar>
+  KOKKOS_INLINE_FUNCTION static
+  Scalar compute_dphi (const Scalar& vtheta_dp, const Scalar& p) {
+    constexpr Real p0    = PhysicalConstants::p0;
+    constexpr Real kappa = PhysicalConstants::kappa;
+    constexpr Real Rgas  = PhysicalConstants::Rgas;
+#ifdef HOMMEXX_BFB_TESTING
+    return (Rgas*vtheta_dp * bfb_pow(p/p0,kappa-1)) / p0;
+#else
+    // TODO: remove temporaries
+    return (Rgas*vtheta_dp * pow(p/p0,kappa-1)) / p0;
+#endif    
   }
 
   // If exner is available, then use exner/p instead of (p/p0)^(k-1)/p0, to avoid dealing with exponentials
