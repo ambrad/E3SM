e972cbc1e39d5816bc0aed8b39b71a606a618e1e
Andrew M. Bradley <ambradl@sandia.gov>
2020-02-16 18:37:41 -0700

    tracer midpoint v

--- components/homme/src/share/sl_advection.F90
+++ components/homme/src/share/sl_advection.F90
@@ -806,8 +806,8 @@ contains
     real(kind=real_kind), intent(out) :: dprecon(np,np,nlev)
 
     real(real_kind), dimension(np,np,nlevp) :: eta0r, eta1r, eta_dot
-    real(real_kind), dimension(np,np) :: ps, ps_t, ptp0, pth, v1, v2, divdp
-    real(real_kind), dimension(np,np,2) :: vdp
+    real(real_kind), dimension(np,np) :: ps, ps_t, ptp0, pth, divdp
+    real(real_kind), dimension(np,np,2) :: vdp, v
     real(real_kind), dimension(np,np,3) :: grad
     real(real_kind), dimension(nlevp) :: hyai_eta, hybi_eta
     real(real_kind) :: dp_neg_min
@@ -871,24 +871,22 @@ contains
             hvcoord%etai(k), grad(:,:,3))
 
        ! Horizontal velocity at time midpoint.
-       k1 = k-1
-       k2 = k
-       v1 = fourth*(elem%state%v(:,:,1,k1,tl%n0 ) + elem%state%v(:,:,1,k2,tl%n0 ) + &
-                    elem%state%v(:,:,1,k1,tl%np1) + elem%state%v(:,:,1,k2,tl%np1))
-       v2 = fourth*(elem%state%v(:,:,2,k1,tl%n0 ) + elem%state%v(:,:,2,k2,tl%n0 ) + &
-                    elem%state%v(:,:,2,k1,tl%np1) + elem%state%v(:,:,2,k2,tl%np1))
+       do d = 1,2
+          v(:,:,d) = fourth*(elem%derived%vstar(:,:,d,k-1) + elem%derived%vstar(:,:,d,k) + &
+                             elem%derived%vn0  (:,:,d,k-1) + elem%derived%vn0  (:,:,d,k))
+       end do
 
        if (amb_fwd /= 0) then
           ! Reconstruct departure level coordinate at final time.
           eta1r(:,:,k) = hvcoord%etai(k) + dt*( &
                eta_dot(:,:,k) + &
-               half*dt*(grad(:,:,1)*v1 + grad(:,:,2)*v2 + grad(:,:,3)*eta_dot(:,:,k)))
+               half*dt*(grad(:,:,1)*v(:,:,1) + grad(:,:,2)*v(:,:,2) + grad(:,:,3)*eta_dot(:,:,k)))
           !eta1r(:,:,k) = max(hvcoord%etai(1), eta1r(:,:,k))
           !eta1r(:,:,k) = min(hvcoord%etai(nlevp), eta1r(:,:,k))
        else
           eta0r(:,:,k) = hvcoord%etai(k) - dt*( &
                eta_dot(:,:,k) - &
-               half*dt*(grad(:,:,1)*v1 + grad(:,:,2)*v2 + grad(:,:,3)*eta_dot(:,:,k)))
+               half*dt*(grad(:,:,1)*v(:,:,1) + grad(:,:,2)*v(:,:,2) + grad(:,:,3)*eta_dot(:,:,k)))
        end if
     end do
     if (amb_fwd /= 0) then
