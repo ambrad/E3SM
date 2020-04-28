de33a96c92480924ed54d570590a5ae66542aecb
Andrew M. Bradley <ambrad@cs.stanford.edu>
2020-04-27 18:28:42 -0600

    move qlt to its own file

---
+++ components/homme/src/share/compose/compose_cedr.hpp
@@ -0,0 +1,15 @@
+#ifndef INCLUDE_COMPOSE_CEDR_HPP
+#define INCLUDE_COMPOSE_CEDR_HPP
+
+#include "cedr.hpp"
+#include "compose.hpp"
+
+namespace homme {
+namespace compose {
+
+namespace ko = Kokkos;
+
+} // namespace compose
+} // namespace homme
+
+#endif
