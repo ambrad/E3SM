959fc448fe0877b04902816c8d798c9122c31c65
Andrew M. Bradley <ambrad@cs.stanford.edu>
2020-08-26 14:07:24 -0600

    make command-line args to tester available to tests

--- components/homme/test/unit_tests/tester.cpp
+++ components/homme/test/unit_tests/tester.cpp
@@ -12,6 +12,11 @@
 
 #include <mpi.h>
 
+// Make command-line arguments available to tests. Anything following "hommexx"
+// in the command-line argument list is passed to the tests.
+int hommexx_catch2_argc;
+char** hommexx_catch2_argv;
+
 int main(int argc, char **argv) {
 
   // Initialize mpi
@@ -19,6 +24,17 @@ int main(int argc, char **argv) {
 
   Homme::Context::singleton().create<Homme::Comm>().reset_mpi_comm(MPI_COMM_WORLD);
   Homme::initialize_hommexx_session();
+
+  // Filter arguments so catch2 doesn't try to interpret hommexx-specific ones.
+  hommexx_catch2_argc = argc;
+  hommexx_catch2_argv = argv;
+  for (int i = 1; i < argc; ++i)
+    if (std::string(argv[i]) == "hommexx") {
+      argc = i;
+      hommexx_catch2_argc -= i;
+      hommexx_catch2_argv = argv + i + 1;
+      break;
+    }
 
   int result = Catch::Session().run(argc, argv);
 
