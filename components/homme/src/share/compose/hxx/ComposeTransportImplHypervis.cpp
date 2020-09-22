30ad5c039d1f141b3da6396933588c9d06c29a46
Andrew M. Bradley <ambrad@cs.stanford.edu>
2020-09-21 20:44:05 -0600

    new file

---
+++ components/homme/src/share/compose/hxx/ComposeTransportImplHypervis.cpp
@@ -0,0 +1,14 @@
+/********************************************************************************
+ * HOMMEXX 1.0: Copyright of Sandia Corporation
+ * This software is released under the BSD license
+ * See the file 'COPYRIGHT' in the HOMMEXX/src/share/cxx directory
+ *******************************************************************************/
+
+#include "ComposeTransportImpl.hpp"
+
+#include "compose_test.hpp"
+
+namespace Homme {
+using CTI = ComposeTransportImpl;
+
+} // namespace Homme
