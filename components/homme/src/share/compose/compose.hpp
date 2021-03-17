2b79f5efcf400192aea341b61bf693cefb62fd47
Andrew M. Bradley <ambradl@sandia.gov>
2021-03-17 09:00:36 -0700

    provide libcompose{f90,c++}.a for optimal COMPOSE_PORT use in, e.g., theta-nlev128 and theta-nlev128-kokkos, respectively

--- components/homme/src/share/compose/compose.hpp
+++ components/homme/src/share/compose/compose.hpp
@@ -29,18 +29,7 @@
 // Look for MPI-related memory leaks.
 //#define COMPOSE_DEBUG_MPI
 
-#ifndef HORIZ_OPENMP
-# ifndef KOKKOS_ENABLE_CUDA
-// Mimic GPU threading on host to debug race conditions on a regular CPU.
-//#  define COMPOSE_MIMIC_GPU
-# endif
-// Optionally define this for testing the port code.
-# define COMPOSE_PORT
-#endif
-
-// Do not modify below here.
-
-#if ! defined COMPOSE_MIMIC_GPU
+#if ! defined COMPOSE_PORT
 # if defined HORIZ_OPENMP
 #  define COMPOSE_HORIZ_OPENMP
 # endif
@@ -49,17 +38,13 @@
 # endif
 #endif
 
-// Define COMPOSE_PORT if we're on or mimicking the GPU or being used by
-// Hommexx. COMPOSE_PORT is used to provide separate code paths when necessary.
-#if ! defined COMPOSE_PORT
-# if defined COMPOSE_MIMIC_GPU || defined KOKKOS_ENABLE_CUDA || defined COMPOSE_WITH_HOMMEXX
-#  define COMPOSE_PORT
-# endif
-#endif
-
 #if defined COMPOSE_PORT
 # if defined COMPOSE_HORIZ_OPENMP || defined COMPOSE_COLUMN_OPENMP
 "This should not happen."
+# endif
+# ifndef KOKKOS_ENABLE_CUDA
+// Mimic GPU threading on host to debug race conditions on a regular CPU.
+//#  define COMPOSE_MIMIC_GPU
 # endif
 # if defined COMPOSE_MIMIC_GPU || defined KOKKOS_ENABLE_CUDA
 // If defined, then certain buffers need explicit mirroring and copying.
