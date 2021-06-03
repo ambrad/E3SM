ab386b85529cd607c2e6e27eb284a8489d5fa35c
Andrew M. Bradley <ambrad@cs.stanford.edu>
2021-06-03 14:41:18 -0600

    gllfvremap: scaffolding

--- components/homme/test_execs/thetal_kokkos_ut/gllfvremap_interface.F90
+++ components/homme/test_execs/thetal_kokkos_ut/gllfvremap_interface.F90
@@ -1,6 +1,46 @@
 module physgrid_interface
+  use kinds, only: real_kind
+  use iso_c_binding, only: c_bool, c_int, c_double, c_ptr, c_loc
+  use dimensions_mod, only: nlev, nlevp, np, nelemd, ne, qsize, qsize_d
+  use geometry_interface_mod, only: par, elem
   implicit none
-
+  
 contains
+
+  subroutine init_gllfvremap_f90(ne, hyai, hybi, hyam, hybm, ps0, dvv, mp, qsize_in, &
+       is_sphere) bind(c)
+    use hybvcoord_mod, only: set_layer_locations
+    use thetal_test_interface, only: init_f90
+    use theta_f2c_mod, only: init_elements_c
+    use edge_mod_base, only: initEdgeBuffer, edge_g
+    use geometry_interface_mod, only: GridVertex
+    use bndry_mod, only: sort_neighbor_buffer_mapping
+    use reduction_mod, only: initreductionbuffer, red_sum, red_min, red_max
+    use parallel_mod, only: global_shared_buf, nrepro_vars
+    use compose_mod, only: compose_init, cedr_set_ie2gci, compose_set_null_bufs
+    use sl_advection, only: sl_init1
+
+    real (real_kind), intent(in) :: hyai(nlevp), hybi(nlevp), hyam(nlev), hybm(nlev)
+    integer (c_int), value, intent(in) :: ne, qsize_in
+    real (real_kind), value, intent(in) :: ps0
+    real (real_kind), intent(out) :: dvv(np,np), mp(np,np)
+    logical (c_bool), value, intent(in) :: is_sphere
+
+    integer :: ie
+
+    if (is_sphere) print *, "NOT IMPL'ED YET"
+
+    qsize = qsize_in
+
+    call init_f90(ne, hyai, hybi, hyam, hybm, dvv, mp, ps0)
+    call init_elements_c(nelemd)
+
+    call initEdgeBuffer(par, edge_g, elem, 6*nlev+1)
+
+    call initReductionBuffer(red_sum,5)
+    call initReductionBuffer(red_min,1)
+    call initReductionBuffer(red_max,1)
+    allocate(global_shared_buf(nelemd, nrepro_vars))
+  end subroutine init_gllfvremap_f90
   
 end module physgrid_interface
---
+++ components/homme/test_execs/thetal_kokkos_ut/gllfvremap_ut.cpp
@@ -0,0 +1,255 @@
+#include "GllFvRemap.hpp"
+
+#include "Types.hpp"
+#include "Context.hpp"
+#include "mpi/Comm.hpp"
+#include "mpi/Connectivity.hpp"
+#include "mpi/MpiBuffersManager.hpp"
+#include "FunctorsBuffersManager.hpp"
+#include "SimulationParams.hpp"
+#include "Elements.hpp"
+#include "Tracers.hpp"
+#include "TimeLevel.hpp"
+#include "HybridVCoord.hpp"
+#include "KernelVariables.hpp"
+#include "PhysicalConstants.hpp"
+#include "ReferenceElement.hpp"
+#include "SphereOperators.hpp"
+#include "ElementOps.hpp"
+#include "profiling.hpp"
+#include "ErrorDefs.hpp"
+#include "VerticalRemapManager.hpp"
+
+#include "utilities/TestUtils.hpp"
+#include "utilities/SyncUtils.hpp"
+#include "utilities/ViewUtils.hpp"
+
+#include <catch2/catch.hpp>
+#include <random>
+
+using namespace Homme;
+
+extern int hommexx_catch2_argc;
+extern char** hommexx_catch2_argv;
+
+extern "C" {
+  void init_gllfvremap_f90(int ne, const Real* hyai, const Real* hybi, const Real* hyam,
+                           const Real* hybm, Real ps0, Real* dvv, Real* mp, int qsize,
+                           bool is_sphere);
+  void init_geometry_f90();
+} // extern "C"
+
+using FA4d = Kokkos::View<Real****, Kokkos::LayoutLeft, Kokkos::HostSpace>;
+using CA4d = Kokkos::View<Real****, Kokkos::LayoutRight, Kokkos::HostSpace>;
+using FA5d = Kokkos::View<Real*****, Kokkos::LayoutLeft, Kokkos::HostSpace>;
+using CA5d = Kokkos::View<Real*****, Kokkos::LayoutRight, Kokkos::HostSpace>;
+
+template <typename V>
+decltype(Kokkos::create_mirror_view(V())) cmvdc (const V& v) {
+  const auto h = Kokkos::create_mirror_view(v);
+  deep_copy(h, v);
+  return h;
+}
+
+template <typename View> static
+Real* pack2real (const View& v) { return &(*v.data())[0]; }
+
+class Random {
+  using rngalg = std::mt19937_64;
+  using rpdf = std::uniform_real_distribution<Real>;
+  using ipdf = std::uniform_int_distribution<int>;
+  std::random_device rd;
+  unsigned int seed;
+  rngalg engine;
+public:
+  Random (unsigned int seed_ = Catch::rngSeed()) : seed(seed_ == 0 ? rd() : seed_), engine(seed) {}
+  unsigned int gen_seed () { return seed; }
+  Real urrng (const Real lo = 0, const Real hi = 1) { return rpdf(lo, hi)(engine); }
+  int  uirng (const int lo, const int hi) { return ipdf(lo, hi)(engine); }
+};
+
+template <typename V>
+void fill (Random& r, const V& a, const Real scale = 1,
+           typename std::enable_if<V::rank == 3>::type* = 0) {
+  const auto am = cmvdc(a);
+  for (int i = 0; i < a.extent_int(0); ++i)
+    for (int j = 0; j < a.extent_int(1); ++j)
+      for (int k = 0; k < a.extent_int(2); ++k)
+        for (int s = 0; s < VECTOR_SIZE; ++s)
+          am(i,j,k)[s] = scale*r.urrng(-1,1); 
+  deep_copy(a, am);
+}
+
+template <typename V>
+void fill (Random& r, const V& a,
+           typename std::enable_if<V::rank == 4>::type* = 0) {
+  const auto am = cmvdc(a);
+  for (int i = 0; i < a.extent_int(0); ++i)
+    for (int j = 0; j < a.extent_int(1); ++j)
+      for (int k = 0; k < a.extent_int(2); ++k)
+        for (int l = 0; l < a.extent_int(3); ++l)
+          for (int s = 0; s < VECTOR_SIZE; ++s)
+            am(i,j,k,l)[s] = r.urrng(-1,1); 
+  deep_copy(a, am);
+}
+
+struct Session {
+  int ne;
+  bool is_sphere;
+  HybridVCoord h;
+  Random r;
+  std::shared_ptr<Elements> e;
+  int nelemd, qsize, nlev, np;
+  FunctorsBuffersManager fbm;
+
+  //Session () : r(269041989) {}
+
+  void init () {
+    const auto seed = r.gen_seed();
+    printf("seed %u\n", seed);
+
+    parse_command_line();
+    assert(is_sphere); // planar isn't available in Hxx yet
+
+    auto& c = Context::singleton();
+
+    c.create<HybridVCoord>().random_init(seed);
+    h = c.get<HybridVCoord>();
+
+    auto& p = c.create<SimulationParams>();
+    p.qsize = qsize;
+    p.params_set = true;
+
+    const auto hyai = cmvdc(h.hybrid_ai);
+    const auto hybi = cmvdc(h.hybrid_bi);
+    const auto hyam = cmvdc(h.hybrid_am);
+    const auto hybm = cmvdc(h.hybrid_bm);
+    auto& ref_FE = c.create<ReferenceElement>();
+    std::vector<Real> dvv(NP*NP), mp(NP*NP);
+    init_gllfvremap_f90(ne, hyai.data(), hybi.data(), &hyam(0)[0], &hybm(0)[0], h.ps0,
+                        dvv.data(), mp.data(), qsize, is_sphere);
+    ref_FE.init_mass(mp.data());
+    ref_FE.init_deriv(dvv.data());
+
+    nelemd = c.get<Connectivity>().get_num_local_elements();
+    auto& bmm = c.create<MpiBuffersManagerMap>();
+    bmm.set_connectivity(c.get_ptr<Connectivity>());
+    e = c.get_ptr<Elements>();
+    c.create<TimeLevel>();
+
+    init_geometry_f90();    
+    auto& geo = c.get<ElementsGeometry>();
+
+    auto& sphop = c.create<SphereOperators>();
+    sphop.setup(geo, ref_FE);
+
+#if 0
+    auto& ct = c.create<ComposeTransport>();
+    ct.reset(p);
+    fbm.request_size(ct.requested_buffer_size());
+    fbm.allocate();
+    ct.init_buffers(fbm);
+    ct.init_boundary_exchanges();
+#endif
+
+    nlev = NUM_PHYSICAL_LEV;
+    assert(nlev > 0);
+    np = NP;
+    assert(np == 4);
+  }
+
+  void cleanup () {
+    auto& c = Context::singleton();
+    c.finalize_singleton();
+  }
+
+  static Session& singleton () {
+    if ( ! s_session) {
+      s_session = std::make_shared<Session>();
+      s_session->init();
+    }
+    return *s_session;
+  }
+
+  // Call only in last line of last TEST_CASE.
+  static void delete_singleton () {
+    if (s_session) s_session->cleanup();
+    s_session = nullptr;
+  }
+
+  Comm& get_comm () const { return Context::singleton().get<Comm>(); }
+
+private:
+  static std::shared_ptr<Session> s_session;
+
+  // compose_ut hommexx -ne NE -qsize QSIZE
+  void parse_command_line () {
+    const bool am_root = get_comm().root();
+    ne = 2;
+    qsize = QSIZE_D;
+    is_sphere = true;
+    bool ok = true;
+    int i;
+    for (i = 0; i < hommexx_catch2_argc; ++i) {
+      const std::string tok(hommexx_catch2_argv[i]);
+      if (tok == "-ne") {
+        if (i+1 == hommexx_catch2_argc) { ok = false; break; }
+        ne = std::atoi(hommexx_catch2_argv[++i]);
+      } else if (tok == "-qsize") {
+        if (i+1 == hommexx_catch2_argc) { ok = false; break; }
+        qsize = std::atoi(hommexx_catch2_argv[++i]);
+      } else if (tok == "-planar") {
+        is_sphere = false;
+      }
+    }
+    ne = std::max(2, std::min(128, ne));
+    qsize = std::max(1, std::min(QSIZE_D, qsize));
+    if ( ! ok && am_root)
+      printf("gllfvremap_ut> Failed to parse command line, starting with: %s\n",
+             hommexx_catch2_argv[i]);
+    if (am_root)
+      printf("gllfvremap_ut> ne %d qsize %d\n", ne, qsize);
+  }
+};
+
+std::shared_ptr<Session> Session::s_session;
+
+static bool almost_equal (const Real& a, const Real& b,
+                          const Real tol = 0) {
+  const auto re = std::abs(a-b)/(1 + std::abs(a));
+  const bool good = re <= tol;
+  if ( ! good)
+    printf("equal: a,b = %23.16e %23.16e re = %23.16e tol %9.2e\n",
+           a, b, re, tol);
+  return good;
+}
+
+static bool equal (const Real& a, const Real& b,
+                   // Used only if not defined HOMMEXX_BFB_TESTING.
+                   const Real tol = 0) {
+#ifdef HOMMEXX_BFB_TESTING
+  if (a != b)
+    printf("equal: a,b = %23.16e %23.16e re = %23.16e\n",
+           a, b, std::abs((a-b)/a));
+  return a == b;
+#else
+  return almost_equal(a, b, tol);
+#endif
+}
+
+typedef ExecViewUnmanaged<Real*[NP][NP][NUM_LEV*VECTOR_SIZE]> RNlev;
+typedef ExecViewUnmanaged<Real**[NP][NP][NUM_LEV*VECTOR_SIZE]> RsNlev;
+typedef ExecViewUnmanaged<Real***[NP][NP][NUM_LEV*VECTOR_SIZE]> RssNlev;
+typedef HostView<Real*[NP][NP][NUM_LEV*VECTOR_SIZE]> RNlevH;
+typedef HostView<Real**[NP][NP][NUM_LEV*VECTOR_SIZE]> RsNlevH;
+typedef HostView<Real***[NP][NP][NUM_LEV*VECTOR_SIZE]> RssNlevH;
+
+TEST_CASE ("compose_transport_testing") {
+  static constexpr Real tol = std::numeric_limits<Real>::epsilon();
+
+  auto& s = Session::singleton(); try {
+    
+    
+  } catch (...) {}
+  Session::delete_singleton();
+}
