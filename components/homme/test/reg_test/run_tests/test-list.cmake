61ef370f9df7a944e7d1645f3d0241cb4029cc03
Andrew M. Bradley <ambrad@cs.stanford.edu>
2023-02-21 19:18:19 -0600

    split preqx and theta-l tests up

--- components/homme/test/reg_test/run_tests/test-list.cmake
+++ components/homme/test/reg_test/run_tests/test-list.cmake
@@ -50,6 +50,8 @@ IF (HOMME_ENABLE_COMPOSE)
     )
 ENDIF()
 
+SET(HOMME_RUN_TESTS_DIR ${HOMME_SOURCE_DIR}/test/reg_test/run_tests)
+
 IF (${BUILD_HOMME_PREQX_KOKKOS})
   # Lists of test files for the HOMME kokkos regression tests.
   # These tests come in pairs, so that, besides checking
@@ -76,12 +78,19 @@ IF (${BUILD_HOMME_PREQX_KOKKOS})
     preqx-nlev72-moist-r3-samenu-tensorhv-lim9-q1
     )
   ENDIF ()
+
+  LIST(APPEND HOMME_TESTS
+    preqx-nhgw-kokkos.cmake
+    preqx-nhgw-slice-kokkos.cmake)
+  IF (HOMMEXX_BFB_TESTING)
+    LIST(APPEND HOMME_ONEOFF_CVF_TESTS
+      preqx-nhgw
+      preqx-nhgw-slice)
+  ENDIF()
 ENDIF()
 
 IF (BUILD_HOMME_THETA_KOKKOS)
-
   # Various one-off tests.
-  SET(HOMME_RUN_TESTS_DIR ${HOMME_SOURCE_DIR}/test/reg_test/run_tests)
   IF (HOMME_ENABLE_COMPOSE)
     LIST(APPEND HOMME_TESTS thetah-sl-test11conv-r0t1-cdr30-rrm-kokkos.cmake)
     IF (HOMMEXX_BFB_TESTING)
@@ -91,8 +100,6 @@ IF (BUILD_HOMME_THETA_KOKKOS)
   ENDIF()
   LIST(APPEND HOMME_TESTS
     thetanh-moist-bubble-kokkos.cmake
-    preqx-nhgw-kokkos.cmake
-    preqx-nhgw-slice-kokkos.cmake
     thetah-nhgw-kokkos.cmake
     thetanh-nhgw-kokkos.cmake
     thetah-nhgw-slice-kokkos.cmake
@@ -100,13 +107,10 @@ IF (BUILD_HOMME_THETA_KOKKOS)
   IF (HOMMEXX_BFB_TESTING)
     LIST(APPEND HOMME_ONEOFF_CVF_TESTS
       thetanh-moist-bubble
-      preqx-nhgw
-      preqx-nhgw-slice
       thetah-nhgw
       thetanh-nhgw
       thetah-nhgw-slice
       thetanh-nhgw-slice)
-    CREATE_CXX_VS_F90_TESTS_WITH_PROFILE(HOMME_ONEOFF_CVF_TESTS short)
   ENDIF()
 
   #cmake/namelist will be built with create-... script
@@ -154,4 +158,10 @@ IF (BUILD_HOMME_THETA_KOKKOS)
   IF (HOMMEXX_BFB_TESTING)
     SET (THETA_COMPARE_F_C_TEST ${HOMME_THETA_TESTS_WITH_PROFILE_1})
   ENDIF ()
+ENDIF()
+
+IF (BUILD_HOMME_PREQX_KOKKOS OR BUILD_HOMME_THETA_KOKKOS)
+  IF (HOMMEXX_BFB_TESTING)
+    CREATE_CXX_VS_F90_TESTS_WITH_PROFILE(HOMME_ONEOFF_CVF_TESTS short)
+  ENDIF()
 ENDIF()
