79bd1d188e0ade2a2ed2c1e250b9ce01d293d562
Andrew M. Bradley <ambradl@sandia.gov>
2024-09-18 20:36:53 -0500

    get compose_ut running again

--- components/homme/test_execs/thetal_kokkos_ut/compose_ut.cpp
+++ components/homme/test_execs/thetal_kokkos_ut/compose_ut.cpp
@@ -143,6 +143,8 @@ struct Session {
     p.scale_factor = is_sphere ? PhysicalConstants::rearth0 : 1;
     p.laplacian_rigid_factor = is_sphere ? 1/p.scale_factor : 0;
 
+#pragma message "THIS CAUSES DIFFS"
+    if (0)
     { // Set hybrid coordinate values to deterministic ones.
       std::vector<Real> hyam(nlev, 0), hyai(nlev+1, 0), hybm(nlev), hybi(nlev+1);
       for (int i = 0; i < nlev+1; ++i) hybi[i] = Real(i)/nlev;
