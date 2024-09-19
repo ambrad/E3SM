95507f57b4fa5f0cfd96e0e7c41da8cadc3c5bf9
Andrew M. Bradley <ambradl@sandia.gov>
2024-09-18 21:59:48 -0500

    find_support and test

--- components/homme/src/share/cxx/ComposeTransportImplEnhancedTrajectory.cpp
+++ components/homme/src/share/cxx/ComposeTransportImplEnhancedTrajectory.cpp
@@ -27,18 +27,65 @@ using S2Nlev  = cti::S2Nlev;
 using R2Nlev  = cti::R2Nlev;
 using S2Nlevp = cti::S2Nlevp;
 
+// For sorted ascending x[0:n] and x in [x[0], x[n-1]] with hint xi_idx, return
+// i such that x[i] <= xi <= x[i+1].
+KOKKOS_FUNCTION static
+int find_support (const int n, const Real* const x, const int x_idx,
+                  const Real xi) {
+  assert(xi >= x[0] and xi <= x[n-1]);
+  // Handle the most common case.
+  if (x_idx < n-1 and xi >= x[x_idx  ] and xi <= x[x_idx+1]) return x_idx;
+  if (x_idx > 0   and xi >= x[x_idx-1] and xi <= x[x_idx  ]) return x_idx-1;
+  // Move on to less common ones.
+  const int max_step = max(x_idx, n-1 - x_idx);
+  for (int step = 1; step <= max_step; ++step) {
+    if (x_idx < n-1-step and xi >= x[x_idx+step  ] and xi <= x[x_idx+step+1])
+      return x_idx+step;
+    if (x_idx > step     and xi >= x[x_idx-step-1] and xi <= x[x_idx-step  ])
+      return x_idx-step-1;
+  }
+  assert(false);
+  return -1;
+}
+
 KOKKOS_FUNCTION static void
-linterp () {
+linterp (const KernelVariables& kv) {
   
 }
 
 void ComposeTransportImpl::calc_enhanced_trajectory (const int np1, const Real dt) {
 }
 
-int ComposeTransportImpl::run_enhanced_trajectory_unit_tests () {
-  printf("hi\n");
-  return 0;
+static int test_find_support () {
+  int ne = 0;
+  const int n = 97;
+  std::vector<Real> x(n);
+  for (int i = 0; i < n; ++i) x[i] = -11.7 + (i*i)/n;
+  const int ntest = 10000;
+  for (int i = 0; i < ntest; ++i) {
+    const Real xi = x[0] + (Real(i)/ntest)*(x[n-1] - x[0]);
+    for (int x_idx : {0, 1, n/3, n/2, n-2, n-1}) {
+      const int sup = find_support(n, x.data(), x_idx, xi);
+      if (sup > n-2) ++ne;
+      else if (xi < x[sup] or xi > x[sup+1]) ++ne;
+    }
+  }
+  return ne;
 }
+
+#define comunittest(f) do {                     \
+    ne = f();                                   \
+    if (ne) printf(#f " ne %d\n", ne);          \
+    nerr += ne;                                 \
+  } while (0)
+
+int ComposeTransportImpl::run_enhanced_trajectory_unit_tests () {
+  int nerr = 0, ne;
+  comunittest(test_find_support);
+  return nerr;
+}
+
+#undef comunittest
 
 } // namespace Homme
 
