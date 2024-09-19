483efa33252df6042b3d836ba99f7848ff2d3e43
Andrew M. Bradley <ambradl@sandia.gov>
2024-09-19 17:21:52 -0500

    dev: draft linterp, first unit test

--- components/homme/src/share/cxx/ComposeTransportImplEnhancedTrajectory.cpp
+++ components/homme/src/share/cxx/ComposeTransportImplEnhancedTrajectory.cpp
@@ -7,10 +7,15 @@
 #include "Config.hpp"
 #ifdef HOMME_ENABLE_COMPOSE
 
+#pragma message "CTET undef NDEBUG"
+#undef NDEBUG
+
 #include "ComposeTransportImpl.hpp"
 #include "PhysicalConstants.hpp"
 
 #include "compose_test.hpp"
+
+#include <random>
 
 namespace Homme {
 using cti = ComposeTransportImpl;
@@ -37,7 +42,7 @@ template <int N> using CRNV = typename ViewConst<RNV<N>>::type;
 // should be used instead.
 template <typename ConstRealArray>
 KOKKOS_FUNCTION static
-int find_support (const int n, ConstRealArray x, const int x_idx,
+int find_support (const int n, const ConstRealArray& x, const int x_idx,
                   const Real xi) {
   assert(xi >= x[0] and xi <= x[n-1]);
   // Handle the most common case.
@@ -55,18 +60,71 @@ int find_support (const int n, ConstRealArray x, const int x_idx,
   return -1;
 }
 
+template <typename ConstRealArray>
+KOKKOS_FUNCTION static Real
+linterp (const int n, const ConstRealArray& x, const ConstRealArray& y,
+         const int xi_idx, const Real xi) {
+  const auto isup = find_support(n, x, xi_idx, xi);
+  const Real a = (xi - x[isup])/(x[isup+1] - x[isup]);
+  return (1-a)*y[isup] + a*y[isup+1];
+}
+
+template <typename Range, typename ConstRealArray, typename RealArray>
+KOKKOS_FUNCTION static void
+linterp (const Range& range, const int n,
+         const ConstRealArray& x, const ConstRealArray& y,
+         const ConstRealArray& xi, RealArray& yi,
+         const char* const caller) {
+#ifndef NDEBUG
+  if (xi[0] < x[0] or xi[n-1] > x[n-1]) {
+    if (caller)
+      printf("linterp: xi out of bounds: %s\n", caller);
+    assert(false);
+  }
+#endif
+  Kokkos::parallel_for(
+    range, [&] (const int k) { yi[k] = linterp(n, x, y, k, xi[k]); });
+}
+
 template <int N>
 KOKKOS_FUNCTION static void
 linterp (const KernelVariables& kv, const int n, const CRNV<N>& x, const CRNV<N>& y,
-         const RNV<N>& xi, const RNV<N>& yi, const char* const caller = nullptr) {
+         const CRNV<N>& xi, const RNV<N>& yi, const char* const caller = nullptr) {
   assert(n <= N);
-  
+  const auto ttr = Kokkos::TeamThreadRange(kv.team, NP*NP);
+  const auto tvr = Kokkos::ThreadVectorRange(kv.team, n);
+  const auto f = [&] (const int idx) {
+    const int i = idx / NP, j = idx % NP;
+    linterp(tvr, n, Homme::subview(x,i,j), Homme::subview(y,i,j),
+            Homme::subview(xi,i,j), Homme::subview(yi,i,j), caller);
+  };
+  Kokkos::parallel_for(ttr, f);
 }
+
+// Public function.
 
 void ComposeTransportImpl::calc_enhanced_trajectory (const int np1, const Real dt) {
 }
 
-static int test_find_support () {
+// Testing.
+
+namespace {
+struct TestData {
+  std::mt19937_64 engine;
+  static const Real eps;
+
+  TestData (const int seed) : engine(seed) {}
+
+  Real urand (const Real lo = 0, const Real hi = 1) {
+    std::uniform_real_distribution<Real> urb(lo, hi);
+    return urb(engine);
+  }
+};
+
+const Real TestData::eps = std::numeric_limits<Real>::epsilon();
+} // namespace
+
+static int test_find_support (TestData&) {
   int ne = 0;
   const int n = 97;
   std::vector<Real> x(n);
@@ -83,15 +141,88 @@ static int test_find_support () {
   return ne;
 }
 
+static void
+run_linterp (const std::vector<Real>& x, const std::vector<Real>& y,
+             std::vector<Real>& xi, std::vector<Real>& yi) {
+  const auto n = x.size();
+  assert(y.size() == n); assert(xi.size() == n); assert(yi.size() == n);
+  // input -> device (test different sizes >= n)
+  ExecView<Real*> xv("xv", n), yv("yv", n+1), xiv("xiv", n+2), yiv("yiv", n+3);
+  const auto xm = Kokkos::create_mirror_view(xv);
+  const auto ym = Kokkos::create_mirror_view(yv);
+  const auto xim = Kokkos::create_mirror_view(xiv);
+  const auto yim = Kokkos::create_mirror_view(yiv);
+  for (size_t i = 0; i < n; ++i) xm(i) = x[i];
+  for (size_t i = 0; i < n; ++i) ym(i) = y[i];
+  for (size_t i = 0; i < n; ++i) xim(i) = xi[i];
+  Kokkos::deep_copy(xv, xm);
+  Kokkos::deep_copy(yv, ym);
+  Kokkos::deep_copy(xiv, xim);
+  // call linterp
+  const auto f = KOKKOS_LAMBDA(const cti::MT& team) {
+    const auto range = Kokkos::TeamVectorRange(team, n);
+    linterp(range, n, xv, yv, xiv, yiv, "unittest");
+  };
+  Homme::ThreadPreferences tp;
+  tp.max_threads_usable = 1;
+  tp.max_vectors_usable = n;
+  tp.prefer_threads = false;
+  tp.prefer_larger_team = true;
+  const auto policy = Homme::get_default_team_policy<ExecSpace>(1, tp);
+  Kokkos::parallel_for(policy, f);
+  Kokkos::fence();
+  // output -> host
+  Kokkos::deep_copy(yim, yiv);
+  for (size_t i = 0; i < n; ++i) yi[i] = yim(i);
+}
+
+static void
+make_random_sorted (TestData& td, const int n, const Real xlo, const Real xhi,
+                    std::vector<Real>& x) {
+  assert(n >= 2);
+  x.resize(n);
+  x[0] = xlo;
+  for (int i = 1; i < n-1; ++i) x[i] = td.urand(xlo, xhi);
+  x[n-1] = xhi;
+  std::sort(x.begin(), x.end());
+}
+
+static int test_linterp (TestData& td) {
+  int nerr = 0;
+  { // Reconstruct a linear function exactly.
+    int ne = 0;
+    const int n = 56;
+    const Real xlo = -1.2, xhi = 3.1;
+    const auto f = [&] (const Real x) { return -0.7 + 1.3*x; };
+    std::vector<Real> x(n), y(n), xi(n), yi(n);
+    for (int trial = 0; trial < 4; ++trial) {
+      make_random_sorted(td, n, xlo, xhi, x);
+      make_random_sorted(td, n,
+                         xlo + (trial == 1 or trial == 3 ?  0.1 : 0),
+                         xhi + (trial == 2 or trial == 3 ? -0.5 : 0),
+                         xi);
+      for (int i = 0; i < n; ++i) y[i] = f(x[i]);
+      run_linterp(x, y, xi, yi);
+      for (int i = 0; i < n; ++i)
+        if (std::abs(yi[i] - f(xi[i])) > 100*td.eps)
+          ++ne;
+    }
+    nerr += ne;
+  }
+  return nerr;
+}
+
 #define comunittest(f) do {                     \
-    ne = f();                                   \
+    ne = f(td);                                 \
     if (ne) printf(#f " ne %d\n", ne);          \
     nerr += ne;                                 \
   } while (0)
 
 int ComposeTransportImpl::run_enhanced_trajectory_unit_tests () {
   int nerr = 0, ne;
+  TestData td(1);
   comunittest(test_find_support);
+  comunittest(test_linterp);
   return nerr;
 }
 
