417acd701440383035fac7c6df49dc72c4d2a05c
Andrew M. Bradley <ambradl@sandia.gov>
2025-09-15 21:01:25 -0500

    use TransportCheckErrorNorms in an older test

--- components/homme/test/reg_test/run_tests/thetah-sl-testconv-3e.cmake
+++ components/homme/test/reg_test/run_tests/thetah-sl-testconv-3e.cmake
@@ -9,3 +9,11 @@ SET(NAMELIST_FILES ${HOMME_ROOT}/test/reg_test/namelists/thetah-sl-testconv-3e.n
 
 # compare all of these files against baselines:
 SET(NC_OUTPUT_FILES dcmip2012_test1_3e_conv1.nc)
+
+set(TCEN_ERROR_ANCHOR "test1_conv")
+set(TCEN_FILENAME "${TEST_NAME}_1.out")
+set(TCEN_UPPER_BOUNDS "1.0e-1;1.1e-1;5.0e-2;9.0e-2")
+configure_file(
+  ${HOMME_SOURCE_DIR}/cmake/TransportCheckErrorNorms.cmake.in
+  ${HOMME_BINARY_DIR}/tests/${TEST_NAME}/check.cmake
+  @ONLY)
